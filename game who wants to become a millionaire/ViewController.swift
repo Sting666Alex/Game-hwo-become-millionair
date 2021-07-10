b9ac57f3a8cefd810355f8cbf5c673e898269b35
Sting666Alex <Sting666alex@gmail.com>
2021-07-10 23:05:56 +0300

    Initial Commit

---
+++ game who wants to become a millionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  game who wants to become a millionaire
+//
+//  Created by Алексей Белов on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
